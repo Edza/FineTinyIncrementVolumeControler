3dcf1bc1e42590bd8718feacd2c2754a0fe90f67
edza <edexdev@gmail.com>
2015-01-07 14:04:58 +0200

    Initial commit

---
+++ FineTinyIncrementVolumeControler/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FineTinyIncrementVolumeControler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int minVol = -1, maxVol = -1, targetVol = -1;
+
+        #region GUI
+
+        bool isAllSet = false;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            minVol = (int)min.Value;
+            maxVol = (int)max.Value;
+            targetVol = (int)vol.Value;
+
+            isAllSet = true;
+            SetVolume();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var item = sender as CheckBox;
+            this.Topmost = item.IsChecked.Value;
+        }
+
+        private void min_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //if(!Validate())
+            //    return;
+
+            minVol = (int)min.Value;
+            ResetVol();
+        }
+
+        private bool Validate()
+        {
+            if (min == null || max == null)
+                return true;
+
+            bool validated = true;
+
+            if (min.Value <= max.Value)
+            {
+                if (max.Value - 10 > -1)
+                    min.Value = max.Value - 10;
+                else
+                {
+                    max.Value = 10;
+                    min.Value = 0;
+                }
+                validated = false;
+            }
+
+            return validated;
+        }
+
+        private void max_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //if (!Validate())
+            //    return;
+
+            maxVol = (int)max.Value;
+            ResetVol();
+        }
+
+        void ResetVol()
+        {
+            if (vol == null)
+                return;
+            vol.Value = 50.0;
+        }
+
+        private void vol_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            targetVol = (int)vol.Value;
+
+            if(minVol != -1 && maxVol != -1)
+                SetVolume();
+        }
+
+        #endregion
+
+        private void SetVolume()
+        {
+            if (!isAllSet)
+                return;
+            int difference = maxVol - minVol;
+            double ration = targetVol / 100.0;
+            double delta = ration * difference;
+            int realVol = minVol + (int)Math.Round(delta);
+            SystemVolumChanger.SetVolume(realVol);
+        }
+    }
+}
---
+++ FineTinyIncrementVolumeControler/VolumeControler.cs
@@ -0,0 +1,70 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Runtime.InteropServices;
+
+namespace FineTinyIncrementVolumeControler
+{
+    internal static class NativeMethods
+    {
+        [DllImport("winmm.dll", EntryPoint = "waveOutSetVolume")]
+        public static extern int WaveOutSetVolume(IntPtr hwo, uint dwVolume);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        public static extern bool PlaySound(string pszSound, IntPtr hmod, uint fdwSound);
+    }
+
+    public static class MSWindowsFriendlyNames
+    {
+        public static string WindowsXP { get { return "Windows XP"; } }
+
+        public static string WindowsVista { get { return "Windows Vista"; } }
+
+        public static string Windows7 { get { return "Windows 7"; } }
+
+        public static string Windows8 { get { return "Windows 8"; } }
+    }
+
+    public static class SystemVolumChanger
+    {
+        public static void SetVolume(int value)
+        {
+            if (value < 0)
+                value = 0;
+
+            if (value > 100)
+                value = 100;
+
+            SetVolumeForWIndowsVista78(value);
+        }
+
+        public static int GetVolume()
+        {
+            int result = 100;
+            try
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator(); // EDataFlow.eRender // ERole.eMultimedia
+                MMDevice device = DevEnum.GetDefaultAudioEndpoint((DataFlow)0, (Role)1);
+                result = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            catch (Exception)
+            {
+            }
+
+            return result;
+        }
+
+        private static void SetVolumeForWIndowsVista78(int value)
+        {
+            try
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                MMDevice device = DevEnum.GetDefaultAudioEndpoint((DataFlow)0, (Role)1);
+
+                device.AudioEndpointVolume.MasterVolumeLevelScalar = (float)value / 100.0f;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
